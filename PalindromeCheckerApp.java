c7311f9e02728903ed51a644cb9e961183ccc126
Indro <70061708+IndronathBasu@users.noreply.github.com>
2026-03-11 14:50:57 +0530

    up2

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,95 +1,23 @@
-import java.util.*;
+import java.util.Scanner;
 
-// Strategy Interface
-interface PalindromeStrategy {
-    boolean isPalindrome(String text);
-}
-
-// Stack Strategy Implementation
-class StackStrategy implements PalindromeStrategy {
-
-    public boolean isPalindrome(String text) {
-
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : text.toCharArray()) {
-            stack.push(c);
-        }
-
-        for (char c : text.toCharArray()) {
-            if (c != stack.pop()) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
-
-// Deque Strategy Implementation
-class DequeStrategy implements PalindromeStrategy {
-
-    public boolean isPalindrome(String text) {
-
-        Deque<Character> deque = new ArrayDeque<>();
-
-        for (char c : text.toCharArray()) {
-            deque.addLast(c);
-        }
-
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
-
-// Context Class
-class PalindromeChecker {
-
-    private PalindromeStrategy strategy;
-
-    public PalindromeChecker(PalindromeStrategy strategy) {
-        this.strategy = strategy;
-    }
-
-    public boolean check(String text) {
-        return strategy.isPalindrome(text);
-    }
-}
-
-// Main Application Class
-public class PalindromeCheckerApp {
+class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter a string:");
-        String input = sc.nextLine();
+        String original = sc.nextLine();
 
-        System.out.println("Choose Strategy:");
-        System.out.println("1. Stack Strategy");
-        System.out.println("2. Deque Strategy");
+        String reversed = "";
 
-        int choice = sc.nextInt();
-
-        PalindromeStrategy strategy;
-
-        if (choice == 1) {
-            strategy = new StackStrategy();
-        } else {
-            strategy = new DequeStrategy();
+        // Reverse the string using a for loop
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
         }
 
-        PalindromeChecker checker = new PalindromeChecker(strategy);
-
-        boolean result = checker.check(input);
-
-        if (result) {
+        // Compare original and reversed string
+        if (original.equals(reversed)) {
             System.out.println("The given string is a Palindrome");
         } else {
             System.out.println("The given string is NOT a Palindrome");
